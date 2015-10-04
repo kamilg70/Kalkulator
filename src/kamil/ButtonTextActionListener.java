dafdfba778de6fcb28f6a4cdd91f15c706082b46
A.G <kgl.1996@wp.pl>
2015-10-04 14:46:09 +0200

    clean up

--- src/kamil/ButtonTextActionListener.java
+++ src/kamil/ButtonTextActionListener.java
@@ -13,15 +13,11 @@ public class ButtonTextActionListener implements ActionListener {
 	public ButtonTextActionListener(JTextField textField, String text) {
 		this.textField = textField;
 		this.text = text;
-		System.out.println(text);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println(textField.getText());
-		System.out.println(text);
 		textField.setText(textField.getText() + text);
-		System.out.println(textField.getText());
 	}
 
 }
